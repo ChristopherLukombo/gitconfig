f9e3cfd7bf943647fdeb2564d7f9d0394140c5d1
Christopher LUKOMBO <christopher.lukombo@outlook.fr>
2023-11-29 22:25:06 +0100

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -1,18 +1,1 @@
-new Date(Date.UTC(date.year, date.month - 1, date.day))
 
-var todayDate = new Date().toISOString().slice(0, 10);
-console.log(todayDate);
-
-https://stackblitz.com/edit/angular-bkuexc?file=src%2Fapp%2Fdatepicker%2Fdatepicker.utc.date.adapter.ts
-
-new Date().toLocaleString("en-US", {timeZone: "America/New_York"})
-var a = '2023-11-12'
- a.split('-');
-var a
- Europe/Paris
- new Date().toLocaleString("en-US", {timeZone: "Europe/Paris"
- const now = new Date();
-    return new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate()));
-https://stackoverflow.com/questions/55118888/how-to-change-ngbdatepicker-format-from-yyyy-mm-dd-to-mm-dd-yyyy-angular-4
-
- https://stackblitz.com/edit/angular-ikdpp5?file=src%2Fapp%2Fdatepicker-adapter.ts
