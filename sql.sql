ba8ba61f5352ab315de1509fb4732d21321cdccd
Christopher LUKOMBO <christopher.lukombo@outlook.fr>
2023-12-26 13:09:58 +0100

    Update sql.sql

--- sql.sql
+++ sql.sql
@@ -2,3 +2,5 @@
 https://linuxhint.com/run-docker-in-wsl-without-docker-desktop/
 
 https://blog.lecacheur.com/2021/11/23/docker-sous-windows-wsl-2-sans-docker-desktop/
+
+https://devicetests.com/remove-docker-from-wsl
