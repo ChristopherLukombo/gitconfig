6064d454528b36a275eb2167f416244051843bf6
Christopher LUKOMBO <christopher.lukombo@outlook.fr>
2023-12-27 22:47:12 +0100

    remove SQL file

--- sql.sql
+++
@@ -1,6 +0,0 @@
-
-https://linuxhint.com/run-docker-in-wsl-without-docker-desktop/
-
-https://blog.lecacheur.com/2021/11/23/docker-sous-windows-wsl-2-sans-docker-desktop/
-
-https://devicetests.com/remove-docker-from-wsl
